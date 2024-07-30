50f3eff3ee0dbb532bf9ca4b3a29cbbacfb51014
datta2345 <161293704+datta2345@users.noreply.github.com>
2024-07-30 14:07:53 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -12,16 +12,16 @@ resource "aws_eks_cluster" "my_cluster" {
   }
 }
 
-resource "aws_eks_node_group" "jawan" {
+resource "aws_eks_node_group" "my_node_group" {
   cluster_name    = aws_eks_cluster.my_cluster.name
-  node_group_name = "jawan"
+  node_group_name = "my_node_group"
   node_role_arn   = "arn:aws:iam::891377358561:role/ec2-admin"  # Replace with your IAM role ARN
   subnet_ids         = ["subnet-06d8a52ca9bd8c56e", "subnet-0f447d6c84c376e9f"]      # Replace with your subnet IDs
 
   scaling_config {
     desired_size = 1
-    max_size     = 4
-    min_size     = 2
+    max_size     = 2
+    min_size     = 1
   }
 
   tags = {
